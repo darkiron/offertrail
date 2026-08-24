f2d5d094d174b70f6fa295ba1cf69aa497286800
Scharpen Vincent <darkiron90@gmail.com>
2026-08-24 15:22:54 +0200

    refactor(frontend): establish global SCSS foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import './styles/global.css';
+import './styles/main.scss';
 
 import App from './App.tsx';
 
